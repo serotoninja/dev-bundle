2ddaca1dce0a3621116133bd5b5db8a90efa7358
serotoninja <serotoninja@gmail.com>
2018-04-13 16:11:02 +0200

    adds bundle php

---
+++ src/SerotoninjaDevBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Serotoninja\DevBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class SerotoninjaDevBundle
+ *
+ * @package Serotoninja\DevBundle
+ *
+ * @author serotoninja <serotoninja@gmail.com>
+ * @since 2018-04-13
+ */
+class SerotoninjaDevBundle extends Bundle
+{
+}
